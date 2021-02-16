eee15e32f9954f71c73e7dedd6824d0119c90739
Pietro Lauria <ptr.lauria@gmail.com>
2021-02-16 11:01:38 +0100

    Second

--- JS101/test.js
+++ JS101/test.js
@@ -1,2 +1,16 @@
-if ((result = someFunction())) {
+const numbers = [1, 2, 3, 4, 5];
+
+function findGreatest(array) {
+  let savedNumber = array[0];
+  if (array === undefined) {
+    return;
+  }
+  array.forEach((num) => {
+    if (num > savedNumber) {
+      savedNumber = num;
+    }
+  });
+
+  return savedNumber;
 }
+console.log(findGreatest());
